ae523e9a254b774ea4b49bce796ab4553580cd5f
Daniel Brice <danielbrice@gmail.com>
2018-05-14 14:02:00 -0700

    solutions for part 1

--- Sets.hs
+++ Sets.hs
@@ -1,31 +1,27 @@
 module Sets where
 
-data Set a = Set {
-  isEmpty :: Bool,
-  contains :: a -> Bool,
-  insert :: a -> Set a,
-  union :: Set a -> Set a
-  }
+data Set a = Set { isEmpty :: Bool, contains :: a -> Bool }
 
-emptySet :: Set a
-emptySet = undefined
+empty :: Set a
+empty = Set True (\x -> False)
 
-totalSet :: Set a
-totalSet = undefined
+total :: Set a
+total = Set False (\x -> True)
 
-insertedSet :: Set a -> a -> Set a
-insertedSet x s = undefined
+insert :: Eq a => Set a -> a -> Set a
+insert s x0 = Set False (\x -> x == x0 || s `contains` x)
 
-unionedSet :: Set a -> Set a -> Set a
-unionedSet s1 s2 = undefined
+union :: Set a -> Set a -> Set a
+union s1 s2 = Set (isEmpty s1 && isEmpty s2)
+                  (\x -> s1 `contains` x || s2 `contains` x)
 
 main :: IO ()
 main = do
-  let oneThreeFive = emptySet `insert` 3
-                              `union` (emptySet `insert` 1)
-                              `insert` 5
+  let oneThreeFive = empty `insert` 3
+                           `union` (empty `insert` 1)
+                           `insert` 5
 
-  let evens = undefined
+  let evens = Set { isEmpty = False, contains = \x -> even x }
 
   putStrLn ("oneThreeFive contains 4: " ++ show (oneThreeFive `contains` 4))
   putStrLn ("evens contains 4: " ++ show (evens `contains` 4))
